46e4930e664b6ee869d8ed8b68079be4fdc23a9a
luxiaohua79112 <2489186909@qq.com>
2022-12-13 17:29:28 +0800

    BUG FIX: IOT-357 open setting locaion service dialog

--- app/src/main/java/io/agora/iotlinkdemo/models/device/add/DeviceAddStep2ResetActivity.java
+++ app/src/main/java/io/agora/iotlinkdemo/models/device/add/DeviceAddStep2ResetActivity.java
@@ -1,11 +1,18 @@
 package io.agora.iotlinkdemo.models.device.add;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.lifecycle.ViewModelProvider;
 
 import io.agora.iotlinkdemo.R;
@@ -14,10 +21,17 @@ import io.agora.iotlinkdemo.base.PermissionHandler;
 import io.agora.iotlinkdemo.base.PermissionItem;
 import io.agora.iotlinkdemo.common.Constant;
 import io.agora.iotlinkdemo.databinding.ActivityResetDeviceBinding;
+import io.agora.iotlinkdemo.dialog.CommonDialog;
 import io.agora.iotlinkdemo.manager.PagePathConstant;
 import io.agora.iotlinkdemo.manager.PagePilotManager;
 import io.agora.iotlinkdemo.models.device.DeviceViewModel;
+import io.agora.iotlinkdemo.utils.ZXingUtils;
+
+import com.agora.baselibrary.base.BaseDialog;
+import com.agora.baselibrary.utils.ToastUtils;
 import com.alibaba.android.arouter.facade.annotation.Route;
+
+import java.io.IOException;
 
 /**
  * 重置设备
@@ -29,6 +43,7 @@ public class DeviceAddStep2ResetActivity extends BaseViewBindingActivity<Activit
     implements PermissionHandler.ICallback  {
 
     private static final String TAG = "LINK/DevAddStep2Act";
+    private static final int REQUEST_CODE_LOCATION_SRV = 1001;
 
     /**
      * 设备模块统一ViewModel
@@ -65,18 +80,28 @@ public class DeviceAddStep2ResetActivity extends BaseViewBindingActivity<Activit
     }
 
     void onBtnNext() {
-        //
-        // ACCESS_FINE_LOCATION 权限判断处理
-        //
+        boolean enableLocation = isLocationProviderEnabled();
+        if (!enableLocation) {
+            showRequestLocationDialog();
+            return;
+        }
+
+        requestAFLPermission();
+    }
+
+    /**
+     * @brief ACCESS_FINE_LOCATION 权限判断处理
+     */
+    void requestAFLPermission() {
         int[] permIdArray = new int[1];
         permIdArray[0] = PermissionHandler.PERM_ID_FINELOCAL;
         mPermHandler = new PermissionHandler(this, this, permIdArray);
         if (!mPermHandler.isAllPermissionGranted()) {
-            Log.d(TAG, "<onBtnNext> requesting permission...");
+            Log.d(TAG, "<requestAFLPermission> requesting permission...");
             mPermHandler.requestNextPermission();
 
         } else {
-            Log.d(TAG, "<onBtnNext> permission granted, goto WIFI activity");
+            Log.d(TAG, "<requestAFLPermission> permission granted, goto WIFI activity");
             PagePilotManager.pageSetDeviceWifi();
         }
     }
@@ -101,4 +126,71 @@ public class DeviceAddStep2ResetActivity extends BaseViewBindingActivity<Activit
             popupMessage(getString(R.string.no_permission));
         }
     }
+
+
+
+    /**
+     * @brief 判断是否开启了GPS或网络定位开关
+     */
+    public boolean isLocationProviderEnabled() {
+        boolean result = false;
+        LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        if (locationManager == null) {
+            return false;
+        }
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            result = true;
+        }
+        return result;
+    }
+
+
+    /**
+     * @brief 显示跳转到 定位服务-->位置信息 系统设置的提示
+     */
+    public void showRequestLocationDialog() {
+        Log.d(TAG, "<showRequestLocationDialog> request FloatWnd permission");
+        if (commonDialog == null) {
+            commonDialog = new CommonDialog(this);
+            commonDialog.setDialogTitle("请给软件设置定位服务位置信息权限，否则不能获取WIFI信息");
+            commonDialog.setDialogBtnText(getString(R.string.cancel), getString(R.string.confirm));
+            commonDialog.setOnButtonClickListener(new BaseDialog.OnButtonClickListener() {
+                @Override
+                public void onLeftButtonClick() {
+                    Log.d(TAG, "<showRequestLocationDialog> onLeftButtonClick");
+                }
+
+                @RequiresApi(api = Build.VERSION_CODES.M)
+                @Override
+                public void onRightButtonClick() {
+                    Log.d(TAG, "<showRequestLocationDialog> onRightButtonClick");
+
+                    // 跳转到系统界面
+                    Intent intentLocalSrv = new Intent();
+                    intentLocalSrv.setAction(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivityForResult(intentLocalSrv, REQUEST_CODE_LOCATION_SRV);
+                }
+            });
+        }
+        commonDialog.show();
+    }
+
+    /**
+     * Handle the return results from the album.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "<onActivityResult> requestCode=" + requestCode
+                + ", resultCode=" + resultCode);
+
+        if (requestCode == REQUEST_CODE_LOCATION_SRV) {
+            boolean enableLocation = isLocationProviderEnabled();
+            if (enableLocation) {   // 已经打开 "位置信息"
+                requestAFLPermission();
+            }
+        }
+    }
+
 }
